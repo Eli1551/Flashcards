de192e4ab89b6360eda068ad27aed8fea7eaec3d
Eli1551 <00001111076613sp@al.educacao.sp.gov.br>
2024-11-27 11:08:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Dragon ball',
+    'Quantas esferas do dragão existem?',
+    'Ao todo, existem 7 esferas do dragão'
+)
+
+criaCartao(
+    'Batman',
+    'Qual foi o nome do primeiro Robin?',
+    'Dick Grayson'
+)
+
+criaCartao(
+    'JoJo',
+    'Quantos JoJos existiram?',
+    'Ao todo, foram 8'
+)
+
+criaCartao(
+    'Naruto',
+    'Qual o nome do poder vital dos ninja?',
+    'Chakra'
+)
+
+criaCartao(
+    'Marvel',
+    'Que personagens ja ganharam os poderes do Thor?',
+    'Jane Foster e Bill Raio Beta'
+)
+
+criaCartao(
+    'Dragon Ball',
+    'Que personagem foi o primeiro a morrer na Saga Sayajin?',
+    'Yamcha'
+)
+
+criaCartao(
+    'Marvel/DC',
+    'Quais são as entidades supremas da Marvel e DC?',
+    'Marvel:One Above All. DC:O Presença'
+)
